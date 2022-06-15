621251f4766c59d4865573104b163c88ffb0d74f
dmfxyz <d@dmf.xyz>
2022-06-14 21:56:42 -0400

    simplify

--- src/runner.js
+++ src/runner.js
@@ -11,13 +11,9 @@ runner.run = async (output_dir) => {
     const multifab_pack = require('../lib/multifab/pack/multifab_full_hardhat.dpack.json')
     const dapp = await dpack.load(multifab_pack, ethers, signer)
     const multifab = await dapp._types.Multifab.deploy()
-
     fs.readdirSync(output_dir).filter((f) => f.endsWith('.snek')).forEach(async(file) => {
         const [abi, bytecode] = fs.readFileSync(`${output_dir}/${file}`, {encoding: 'utf-8'}).split("\n")
-        console.log(bytecode);
-        // TODO: DMFXYZ Bytecode is not being passed correctly
-        const codehash = await multifab.cache(bytecode)
-        console.log(codehash);
-        console.log(`Cached ${file} to multifab with codehash: ${codehash}`)
+        const cache_tx = await multifab.cache(bytecode)
+        console.log(cache_tx)
     });
 }
