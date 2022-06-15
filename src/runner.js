040171174fc8a144b58a8e9e1e2365e667b7a259
dmfxyz <d@dmf.xyz>
2022-06-14 23:12:06 -0400

    parse and store codehash

--- src/runner.js
+++ src/runner.js
@@ -1,6 +1,7 @@
 const dpack = require('@etherpacks/dpack')
 const ethers = require('ethers');
 const ganache = require("ganache")
+const { send } = require('minihat')
 const fs = require('fs')
 
 module.exports = runner = {}
@@ -11,9 +12,14 @@ runner.run = async (output_dir) => {
     const multifab_pack = require('../lib/multifab/pack/multifab_full_hardhat.dpack.json')
     const dapp = await dpack.load(multifab_pack, ethers, signer)
     const multifab = await dapp._types.Multifab.deploy()
+    const contracts = {}
     fs.readdirSync(output_dir).filter((f) => f.endsWith('.snek')).forEach(async(file) => {
         const [abi, bytecode] = fs.readFileSync(`${output_dir}/${file}`, {encoding: 'utf-8'}).split("\n")
-        const cache_tx = await multifab.cache(bytecode)
-        console.log(cache_tx)
+        const cache_tx = await send(multifab.cache, bytecode);
+        [,codehash] = cache_tx.events.find(event => event.event === 'Added').args
+        contracts[file] = {
+            codehash: codehash
+        }
+        console.log(contracts)
     });
 }
